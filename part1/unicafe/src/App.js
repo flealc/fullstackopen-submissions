acb6c07d4839000bcf4e53649f31afea2e1db1c6
Fidel Leal <fidelxleal@gmail.com>
2023-02-27 09:29:44 -0600

    add unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,7 +12,7 @@ const Header = (props) => (
 )
 
 const Counter = (props) => (
-  <div>{props.text} {props.feedbackType}</div>
+  <div>{props.text} {props.display}</div>
 )
 
 
@@ -21,18 +21,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  // log specific feedback value (1, 0, -1) to an array for average calculation
+  const [average, setAverage] = useState([])
 
+  
+  // click handlers
   const feedbackGood = () => {
     setGood(good + 1)
+    setAverage(average.concat(1))
   }
 
   const feedbackNeutral = () => {
     setNeutral(neutral + 1)
+    setAverage(average.concat(0))
   }
 
   const feedbackBad = () => {
     setBad(bad + 1)
+    setAverage(average.concat(-1))
   }
+
 
   return (
     <div>
@@ -41,10 +49,13 @@ const App = () => {
       <Button handleClick={feedbackNeutral} text={'neutral'}/> 
       <Button handleClick={feedbackBad} text={'bad'}/> 
       <Header text={'statistics'}/>
-      <Counter text={'good'} feedbackType={good}/>
-      <Counter text={'neutral'} feedbackType={neutral}/>
-      <Counter text={'bad'} feedbackType={bad}/>
-
+      <Counter text={'good'} display={good}/>
+      <Counter text={'neutral'} display={neutral}/>
+      <Counter text={'bad'} display={bad}/>
+      <Counter text={'all'} display={good + bad + neutral}/>
+      <Counter text={'average'} display={average.reduce((acc, c) => acc + c, 0) / average.length}/>
+      <Counter text={'positive'} display={good * 100 / (bad + good + neutral) + ' %'}/>
+      
     </div>
   )
 }
