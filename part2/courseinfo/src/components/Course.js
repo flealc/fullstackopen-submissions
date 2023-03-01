7349df7d57d696bcf24a88f6b7cf7b1b5d130930
Fidel Leal <fidelxleal@gmail.com>
2023-03-01 09:55:46 -0600

    add courseinfo step6

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,25 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => 
+        <Part key={part.id} part={part}/>
+    )}
+  </>
+
+
+const Course = ( {course} ) => {
+    return (
+        <>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+        </>
+    )
+}
+
+export default Course
