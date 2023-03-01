195d551ec88116c07dc394f183c23cbbbd8ff88a
Fidel Leal <fidelxleal@gmail.com>
2023-03-01 10:18:25 -0600

    add courseinfo steps7-8

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,25 +1,35 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Part = ({ part }) => 
+const Total = ({ parts }) => {
+  const sum = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <h4>total of {sum} exercises</h4>
+  )
+
+}
+const Part = ({ part }) =>
   <p>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
+const Content = ({ parts }) =>
   <>
-    {parts.map(part => 
-        <Part key={part.id} part={part}/>
+    {parts.map(part =>
+      <Part key={part.id} part={part} />
     )}
   </>
 
 
-const Course = ( {course} ) => {
-    return (
-        <>
-            <Header course={course.name}/>
-            <Content parts={course.parts}/>
-        </>
-    )
+const Course = ({ course }) => {
+
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
 }
 
 export default Course
