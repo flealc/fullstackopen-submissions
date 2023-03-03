a10b794a1eadff0b0cda3462109c22ba0b221797
Fidel Leal <fidelxleal@gmail.com>
2023-03-03 17:21:16 -0600

    add phonebook step8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -11,10 +11,10 @@ const App = () => {
   const [filter, setNewFilter] = useState('')
 
 useEffect(() => {
-  axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      setPersons(response.data)
+  personService
+    .getAll()
+    .then(initialPersons => {
+      setPersons(initialPersons)
     })
 }, [])
 
@@ -40,16 +40,14 @@ useEffect(() => {
         name: newName,
         number: newNumber,
       }
-
-      axios
-        .post('http://localhost:3001/persons', nameObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      
+      personService
+        .create(nameObject)
+        .then(returnedName => {
+          setPersons(persons.concat(returnedName))
           setNewName('')
           setNewNumber('')
         })
-      
-      
     }
   }
 
