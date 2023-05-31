54efbd8498b1f38800092e904db1c8d6236dd9d8
Fidel Leal <flus1717@gmail.com>
2023-05-31 09:23:52 -0500

    add bloglist front end, step1

---
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
+const getAll = async () => {
+  const config = {
+    headers: {Authorization: token}
+  }
+
+  const request = await axios.get(baseUrl, config)
+
+  return  request.data
+}
+
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll, setToken }
