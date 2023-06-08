0543747de46cb337fad597cd89995966836c0610
Fidel Leal <flus1717@gmail.com>
2023-06-08 11:38:41 -0500

    add bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -2,6 +2,12 @@ describe('Blog app', function() {
 
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,4 +15,28 @@ describe('Blog app', function() {
     cy.visit('http://localhost:3000')
     cy.contains('log in to application')
   })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input:first').type('mluukkai')
+      cy.get('input:last').type('salainen')
+      cy.contains('login').click()
+
+      cy.contains('blogs')
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input:first').type('mluukkai')
+      cy.get('input:last').type('b4dp4ssw0rd')
+      cy.contains('login').click()
+
+      cy.get('.error')
+        .should('contain','Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.should('not.contain', 'Matti Luukkainen logged in')
+    })
+  })
+
 })
