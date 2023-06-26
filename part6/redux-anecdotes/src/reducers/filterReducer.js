d4646986a6c7b510eb443ce1500f67fbbd5d8efb
Fidel Leal <flus1717@gmail.com>
2023-06-25 22:31:29 -0500

    add redux-anecdotes. step 8

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,19 @@
-const filterReducer = (state = '', action) => {
-  switch(action.type) {
-    case 'SET':
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = ''
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterChange(state, action) {
+       
       return action.payload
-    default:
-      return state
+    }
   }
 
-}
+})
 
-export const filterChange = filter => {
-  return {
-    type: 'SET',
-    payload: filter
-  }
-}
 
-export default filterReducer
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
