8fd2169db726507c279a3ae56a8c7905a7555992
Fidel Leal <flus1717@gmail.com>
2023-06-28 09:28:28 -0500

    add query-anecdotes, step1

---
+++ part6/query-anecdotes/src/App.js
@@ -0,0 +1,50 @@
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getAnecdotes } from './requests'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+
+  const queryClient = useQueryClient()
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const result = useQuery('anecdotes', getAnecdotes, {
+    refetchOnWindowFocus: false,
+    retry: 1
+  })
+  
+  if ( result.isError || result.isLoading ) {
+    return (
+      <div>
+        anecdote service not available due to problems in server
+      </div>
+    )
+  }
+
+  const anecdotes = result.data
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
